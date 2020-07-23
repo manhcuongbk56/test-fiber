b9a892d1d4ad7d0dd8ef2ca6e8f69485ede4c3b9
cuong <cuong@gmail.com>
2020-07-23 11:27:52 +0700

    .gitignore is now working

--- src/main/java/org/idle/easy/fibers/AppLauncher.java
+++ src/main/java/org/idle/easy/fibers/AppLauncher.java
@@ -7,7 +7,6 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class AppLauncher {
-    private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
 
     public static void main(String[] args) {
         Vertx.vertx().deployVerticle(MainVerticle.class.getName(), h -> {
